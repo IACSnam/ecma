7da7a6c04daf994b056324bd96b484267cd9acd8
IACSnam <56830564+IACSnam@users.noreply.github.com>
2020-03-27 11:29:13 -0400

    yes

--- public/projects/in-progress/coin-rush/main.js
+++ public/projects/in-progress/coin-rush/main.js
@@ -1,3 +1,17 @@
 var canvas = document.getElementById('game');
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight*.95;
+canvas.width = canvas.height*(1280/720)
+var x_factor = canvas.width/1280;
+var y_factor = canvas.height/720;
+
+game = GameCanvas('game');
+const ctx = canvas.getContext('2d');
+
+function main_menu(){
+    //background
+    var background = new Image(); background.src = 'menu_background.png';
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);
+    //menu
+}
+
+main_menu()
